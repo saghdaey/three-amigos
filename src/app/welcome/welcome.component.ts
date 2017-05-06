079169ef145ca6752981217bc60b7ebc4dd3fadd
Sanam Aghdaey <sanamaghdaey@Sanams-MacBook-Pro.local>
2017-05-06 11:14:17 -0400

    add basic welcome component

---
+++ src/app/welcome/welcome.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'welcome',
+  template: `
+    <div>
+      <h1>Welcome Component</h1>
+    </div>
+  `
+})
+export class WelcomeComponent {
+
+}
