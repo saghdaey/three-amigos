0e1e12b1ab05b4b42905237def3bf18fcd25ff4d
Sanam Aghdaey <sanamaghdaey@Sanams-MacBook-Pro.local>
2017-05-06 11:14:32 -0400

    modifying routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,14 +2,15 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './home';
 import { AboutComponent } from './about';
 import { NoContentComponent } from './no-content';
+import { WelcomeComponent} from './welcome';
 
 import { DataResolver } from './app.resolver';
 import { RegistrationComponent } from './registration';
 
 export const ROUTES: Routes = [
-  { path: '',      component: HomeComponent },
+  { path: '',      component: WelcomeComponent },
   { path: 'home',  component: HomeComponent },
-  { path: 'register', component: RegistrationComponent},
+  { path: 'register', component: RegistrationComponent} ,
   { path: 'about', component: AboutComponent },
   { path: 'detail', loadChildren: './+detail#DetailModule'},
   // { path: 'barrel', loadChildren: './+barrel#BarrelModule'},
