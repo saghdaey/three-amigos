f0efe72f9e91f34753e5a35a0962e89a4867c37f
Sanam Aghdaey <sanamaghdaey@Sanams-MacBook-Pro.local>
2017-05-06 11:17:35 -0400

    turn off web pack warning logs

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -235,6 +235,9 @@ module.exports = function (options) {
       watchOptions: {
         aggregateTimeout: 300,
         poll: 1000
+      },
+      stats:{
+        warnings:false
       }
     },
 
